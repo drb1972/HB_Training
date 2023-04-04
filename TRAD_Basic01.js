c1c7ca0724c5d111f74aaa743a4f1581710d5900
Diego Rodriguez <diego.rodriguez@broadcom.com>
2023-04-04 19:38:09 +0200

    c1

--- TRAD_Basic01.js
+++ TRAD_Basic01.js
@@ -1,30 +1,26 @@
 // TRAD Basic01
+// Lo más básico posible
 // Copy into Eclipse as an TRAD_Basic01.hbx file
-// http://mstrsvw.lvn.broadcom.net:8000/hbscript/getQ_drb
+// http://mstrsvw.lvn.broadcom.net:8000/hbscript/TRAD_Basic01
 
 var hb = new HB.Session();
-var common = require('common', 'hbutils');
-var debugging = require('debugging', 'hbutils');
-common.hbRunProto();
-debugging.checkDebugControl();
-debugging.global = true;
 
 hb.set('hb_entry', 'trad');
 hb.set('hb_timeout', '300');
-hb.run('HB_AID=ENTER', 'T002');
+hb.run('HB_AID=ENTER');
 
 hb.set('OPTION', '1');
-hb.run('HB_AID=ENTER', 'T003');
+hb.run('HB_AID=ENTER');
 
 hb.set('OPT2', '1');
-hb.run('HB_AID=ENTER', 'T004');
+hb.run('HB_AID=ENTER');
 
 writeln('company = ', hb.getFieldValue('COMP41'));
 writeln('value   = ', hb.getFieldValue('SHRNOW'));
-writeln('help    =' , hb.getFieldValue('HELD'));
+writeln('held    = ', hb.getFieldValue('HELD'));
 writeln('value   = ', hb.getFieldValue('VALUE'));
-hb.run('HB_AID=PF3', 'T003');
 
-hb.run('HB_AID=PF3', 'T002');
+hb.run('HB_AID=PF3');
+hb.run('HB_AID=PF3');
 
 hb.run('HB_AID=PF12&hb_delete_session=1');
